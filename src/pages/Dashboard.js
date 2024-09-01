3bcabd2ffb0c8ab79450de1ac77fa4389e9c5f06
Sidath <sidathasiri@gmail.com>
2024-09-01 10:29:32 +0530

    add loader

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Typography, Row, Col } from "antd";
+import { Typography, Row, Col, Spin } from "antd";
 import ProjectDropdown from "../components/ProjectDropdown";
 
 const { Title } = Typography;
 
-const Dashboard = ({ projects }) => {
+const Dashboard = ({ projects }) => {  
   return (
     <div
       style={{
@@ -16,23 +16,35 @@ const Dashboard = ({ projects }) => {
       }}
     >
       <Row justify="center" align="top">
-        <Col>
-          <Title
-            level={2}
-            style={{ textAlign: "center", marginBottom: "20px" }}
-          >
-            Select a Project
-          </Title>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "flex-start",
-            }}
-          >
-            <ProjectDropdown projects={projects} />
-          </div>
-        </Col>
+        {!projects || projects.length === 0 ? (
+          <Spin tip="Loading">
+            <div
+              style={{
+                padding: 50,
+                background: "rgba(0, 0, 0, 0.05)",
+                borderRadius: 4,
+              }}
+            />
+          </Spin>
+        ) : (
+          <Col>
+            <Title
+              level={2}
+              style={{ textAlign: "center", marginBottom: "20px" }}
+            >
+              Select a Project
+            </Title>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+              }}
+            >
+              <ProjectDropdown projects={projects} />
+            </div>
+          </Col>
+        )}
       </Row>
     </div>
   );
